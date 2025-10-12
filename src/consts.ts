59dcde5aad32c4288b76e862a4b3d6106a284e6c
nobu-yone <soraironoyume24@gmail.com>
2025-10-12 20:36:39 +0900

    index.astro Layout.astro の作成とトップページのレイアウト化

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Nobu Yone's Portfolio";
 export const SITE_DESCRIPTION = 'Yoncoachのポートフォリオサイトです';
+export const JOB_TITLE = 'Web Developer and UX Designer'
